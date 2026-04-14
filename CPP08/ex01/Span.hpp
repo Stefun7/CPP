65a95162956c94dbfb00da15718fbb9b129ca17f
Stephen Cesar <scesar@shi-r10-p2.s19.be>
2026-04-14 15:05:06 +0200

    08 almost finished

--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,14 +6,16 @@
 /*   By: scesar <scesar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 13:31:26 by scesar            #+#    #+#             */
-/*   Updated: 2026/04/03 19:49:49 by scesar           ###   ########.fr       */
+/*   Updated: 2026/04/14 15:04:31 by scesar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 # include <iostream>
 # include <vector>
+# include <list>
 # include <algorithm>
+# include <iterator>
 # include <limits>
 
 class Span{
@@ -25,6 +27,17 @@ public:
 	Span &operator=(Span const &S);
 
 	void addNumber(int n);
+
+	template <typename it>
+	void addNumbers(it start, it end){
+	size_t dist = std::distance(start, end);
+
+	if(this->_vec.size() + dist > this->_max_sz)
+		throw(WillOverflow());
+	this->_vec.insert(this->_vec.end(), start, end);
+}
+	//Template functions MUST be defined in the header file because the compiler needs to see the full implementation at the moment it uses the template.
+
 	int shortestSpan();
 	int longestSpan();
 
@@ -32,6 +45,12 @@ public:
 	public:
 		const char *what() const throw(){
 			return "Already full !";
+		}
+	};
+	class WillOverflow : public std::exception{
+	public:
+		const char *what() const throw(){
+			return "Not enough space !";
 		}
 	};
 	class NoSpan : public std::exception{
--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: scesar <scesar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/03 19:48:19 by scesar            #+#    #+#             */
-/*   Updated: 2026/04/03 19:48:40 by scesar           ###   ########.fr       */
+/*   Created: 2026/04/14 14:52:21 by scesar            #+#    #+#             */
+/*   Updated: 2026/04/14 14:53:31 by scesar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,70 @@ sp.addNumber(3);
 sp.addNumber(17);
 sp.addNumber(9);
 sp.addNumber(11);
-std::cout << sp.shortestSpan() << std::endl;
-std::cout << sp.longestSpan() << std::endl;
-return 0;
+std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+	std::cout << "Longest span:  " << sp.longestSpan() << std::endl;
+
+	std::cout << "\n=== Overflow test ===" << std::endl;
+	try
+	{
+		sp.addNumber(42); // should throw
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+
+	std::cout << "\n=== Range test (vector) ===" << std::endl;
+	Span sp2(10);
+
+	std::vector<int> v;
+	v.push_back(100);
+	v.push_back(200);
+	v.push_back(300);
+	v.push_back(400);
+	v.push_back(500);
+	sp2.addNumbers(v.begin(), v.end());
+
+	std::cout << "Shortest span: " << sp2.shortestSpan() << std::endl;
+	std::cout << "Longest span:  " << sp2.longestSpan() << std::endl;
+
+	std::cout << "\n=== Range test (list) ===" << std::endl;
+	std::list<int> lst;
+	lst.push_back(7);
+	lst.push_back(3);
+	lst.push_back(20);
+	lst.push_back(1);
+
+	Span sp3(10);
+	sp3.addNumbers(lst.begin(), lst.end());
+
+	std::cout << "Shortest span: " << sp3.shortestSpan() << std::endl;
+	std::cout << "Longest span:  " << sp3.longestSpan() << std::endl;
+
+	std::cout << "\n=== Big test ===" << std::endl;
+	Span big(10000);
+
+	std::vector<int> bigVec;
+	for (int i = 0; i < 10000; ++i)
+		bigVec.push_back(i * 2);
+
+	big.addNumbers(bigVec.begin(), bigVec.end());
+
+	std::cout << "Shortest span: " << big.shortestSpan() << std::endl;
+	std::cout << "Longest span:  " << big.longestSpan() << std::endl;
+
+	std::cout << "\n=== Edge case: not enough numbers ===" << std::endl;
+	Span sp4(5);
+
+	try
+	{
+		sp4.addNumber(42);
+		std::cout << sp4.shortestSpan() << std::endl; // should throw
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+
+	return 0;
 }
