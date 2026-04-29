e38e29d9a0e9e508396c9b47f71c67c05bab269a
Stephen Cesar <scesar@fu-r10-p5.s19.be>
2026-04-29 19:09:45 +0200

    litlle adjustement

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,12 +6,30 @@
 /*   By: scesar <scesar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 16:44:54 by scesar            #+#    #+#             */
-/*   Updated: 2026/04/23 17:09:42 by scesar           ###   ########.fr       */
+/*   Updated: 2026/04/29 18:58:50 by scesar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 # include <iostream>
 # include <fstream>
+# include <exception>
 # include <map>
 
+class InvalidFile : public std::exception{
+	const char* what() const throw();
+};
+
+class WrongFormat : public std::exception{
+	const char* what() const throw();
+};
+
+class InvalidDate : public std::exception{
+	const char* what() const throw();
+};
+
+class InvalidValue : public std::exception{
+	const char* what() const throw();
+};
+
+std::map<std::string, float> csv_parser();
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: scesar <scesar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 17:09:49 by scesar            #+#    #+#             */
-/*   Updated: 2026/04/23 17:10:44 by scesar           ###   ########.fr       */
+/*   Updated: 2026/04/29 18:58:08 by scesar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int main(int ac, char *av)
 {
+	if(ac != 2)
+	{
+		std::cerr << "Wrong argument number, you need exactly one file !" << std::endl;
+		return(1);
+	}
+	try{
+			std::map<std::string, float> data = csv_parser();
+	}
+	catch(std::exception &e)
+	{
 
+	}
 }
