95f3bb5c7e0cbc83d42a039c2f0c11fef50f6427
Stephen Cesar <scesar@fu-r9-p5.s19.be>
2026-05-12 15:17:21 +0200

    ...

---
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scesar <scesar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/12 15:03:34 by scesar            #+#    #+#             */
+/*   Updated: 2026/05/12 15:17:05 by scesar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+std::map<std::string, float> csv_parser(std::string file){
+	std::ifstream data;
+	std::map<std::string, float> data_map;
+	std::string line;
+
+	//check_file_name
+	//open file with data.open(file);
+	//while(getline(data, line))=>check line
+
+}
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,24 +6,25 @@
 /*   By: scesar <scesar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 17:09:49 by scesar            #+#    #+#             */
-/*   Updated: 2026/04/29 18:58:08 by scesar           ###   ########.fr       */
+/*   Updated: 2026/05/12 15:03:23 by scesar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main(int ac, char *av)
+int main(int ac, char **av)
 {
 	if(ac != 2)
 	{
 		std::cerr << "Wrong argument number, you need exactly one file !" << std::endl;
 		return(1);
 	}
-	try{
-			std::map<std::string, float> data = csv_parser();
+	try
+	{
+			std::map<std::string, float> data = csv_parser(av[1]);
 	}
 	catch(std::exception &e)
 	{
-
+		std::cout << "Exception caught: " << e.what() << std::endl;
 	}
 }
